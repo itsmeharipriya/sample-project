8ae48f36aa593802fd404e2c58d5ccd3fe472f6d
Haripriya <haripriyaammu2001@gmail.com>
2025-07-05 15:18:24 +0530

    Added existing project folder: CucumberPro

---
+++ CucumberPro/src/main/java/cucumber_project/CucumberPro/App.java
@@ -0,0 +1,10 @@
+package cucumber_project.CucumberPro;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
